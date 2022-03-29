0e557b484bda4cca4082baf3aecd9146898905dc
Patrick Ngobiro <pngobiro@gmail.com>
2022-03-29 15:41:25 +0300

    initial commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -122,22 +122,22 @@
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                                    
-                        
-                                    @if (Auth::user()==null)
-                                    
-                                    @else
-                                    <p>  PJ: {{ Auth::user()->jsg_number }} </p>
-                                    @endif
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">
                                 
+                    
+                                @if (Auth::user()==null)
                                 
-                                </span>
-                                
-                                <img class="img-profile rounded-circle"
+                                @else
+                                <p>  PJ: {{ Auth::user()->jsg_number }} </p>
+                                @endif
+                            
+                            
+                            </span>
+                            
+                            <img class="img-profile rounded-circle"
                                 src="img/undraw_profile.svg">
-                            </a>
+                        </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
