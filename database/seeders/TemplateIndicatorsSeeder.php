87d46a66d1e14de463e60f4b83901231fbf8091f
Patrick Ngobiro <pngobiro@gmail.com>
2022-05-31 11:28:27 +0300

    template indicator seeder update

--- database/seeders/TemplateIndicatorsSeeder.php
+++ database/seeders/TemplateIndicatorsSeeder.php
@@ -23,13 +23,13 @@ class TemplateIndicatorsSeeder extends Seeder
                 TemplateIndicator::create([
                     "id" =>                             $data['0'],
                     "master_indicator_id" =>            $data['1'],
-                    "name" =>                           $data['3'],
-                    "indicator_group_id" =>             $data['6'],
-                    "indicator_type_id" =>              $data['7'],
-                    "indicator_unit_of_measure_id" =>   $data['8'],
-                    "indicator_weight" =>               $data['9'],
-                    "is_backlog_indicator" =>           $data['12'],
-                    "order" =>                          $data['15'],
+                    "name" =>                           $data['2'],
+                    "indicator_group_id" =>             $data['5'],
+                    "indicator_type_id" =>              $data['6'],
+                    "indicator_unit_of_measure_id" =>   $data['7'],
+                    "indicator_weight" =>               $data['8'],
+                    "is_backlog_indicator" =>           $data['11'],
+                    "order" =>                          $data['14'],
                 
                 ]);  
             }
