3dd240658fb4bece34b65af0afde029b7d6f4c04
Patrick Ngobiro <pngobiro@gmail.com>
2022-05-31 10:58:17 +0300

    user management module update

--- database/migrations/2022_03_17_101738_create_role_user_table.php
+++ database/migrations/2022_03_17_101738_create_role_user_table.php
@@ -12,9 +12,9 @@ class CreateRoleUserTable extends Migration
             $table->primary(['role_id', 'user_id']);
             $table->foreignId('role_id')->constrained()->cascadeOnDelete();
             $table->dateTime('deleted_at')->nullable();
-            //$table->foreignId('user_id')->constrained()->cascadeOnDelete();
-          
-
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            // drop cascade user_id
+            
 
         });
     }
